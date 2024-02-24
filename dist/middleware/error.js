345f355c954672249f8ee04f482a779183ea6da0
Ranjan Prabhu <ranjanprithvi@gmail.com>
2024-02-24 19:39:43 +0100

    added dist to gitignore

--- dist/middleware/error.js
+++
@@ -1,5 +0,0 @@
-import { logger } from "../startup/logger.js";
-export function error(err, req, res, next) {
-    logger.error(err.message, err);
-    res.status(500).send("Something went wrong..");
-}
--- dist/models/authModel.js
+++
@@ -1,6 +0,0 @@
-import Joi from "joi";
-const authSchema = {
-    email: Joi.string().email().max(255).required(),
-    password: Joi.string().max(1024).required(),
-};
-export const authSchemaObject = Joi.object(authSchema);
